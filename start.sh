e5de38e2d2de7d3339c4610724bdac0113f5e77a
Hanyu <8245snake@gmail.com>
2020-04-22 22:49:02 +0900

    バグ修正

--- start.sh
+++ start.sh
@@ -10,9 +10,9 @@ cd ${GOPATH}/src/github.com
 git clone https://github.com/8245snake/bikeshare_api.git
 chmod -R 777 $HOME
 
-cd ${$HOME}/src/${BINARY_NAME}
+cd ${HOME}/src/${BINARY_NAME}
 make
 
-cd ${$HOME}/app/bin/${BINARY_NAME}
+cd ${HOME}/app/bin/${BINARY_NAME}
 
 ${BINARY_NAME}
