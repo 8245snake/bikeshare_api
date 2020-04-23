a00047f6615371395fcc418b11cffdf31752906f
Hanyu <8245snake@gmail.com>
2020-04-23 20:51:55 +0900

    ビルドスクリプト修正

--- start.sh
+++ start.sh
@@ -2,20 +2,25 @@
 
 HOME=${GOPATH}/src/github.com/bikeshare_api
 
+# ブランチをチェック
 if [ -e $HOME ]; then
-    rm -r $HOME
+    cd $HOME
+    git checkout .
+    git pull origin master
+else
+    cd ${GOPATH}/src/github.com
+    git clone https://github.com/8245snake/bikeshare_api.git
 fi
-
-cd ${GOPATH}/src/github.com
-git clone https://github.com/8245snake/bikeshare_api.git
 chmod -R 777 $HOME
 
+# ビルドする
 cd ${HOME}/src/${BINARY_NAME}
 make
 
-cd ${HOME}/app/bin
+# ビルド成果物を移動
 chmod -R 777 $HOME
-
 cp -R -f  ${HOME}/ /usr/
 
-#exec ./${BINARY_NAME}
+# 実行
+cd /usr/bikeshare_api/app/bin
+exec ./${BINARY_NAME}
