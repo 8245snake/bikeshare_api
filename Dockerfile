211169a20dd7c9cd7e99fccbf6e7fa84b3f8f8c3
Hanyu <8245snake@gmail.com>
2020-04-25 00:13:40 +0900

    ビルドスクリプトを更新

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,12 @@ FROM golang:latest
 WORKDIR /usr/
 
 RUN apt-get update && apt-get install -y \
-	git \
-	--no-install-recommends
+    bash \
+    git \
+    --no-install-recommends
 RUN go get github.com/kr/godep
 
 ADD ./start.sh /go/
 RUN chmod 777 /go/start.sh
 
-CMD ["/bin/sh", "/go/start.sh"]
+CMD ["bash", "/go/start.sh"]
--- start.sh
+++ start.sh
@@ -1,26 +1,48 @@
-#!/bin/sh
+#!/bin/bash
+# ビルド用コンテナと実行用コンテナで共通利用する
 
 HOME=${GOPATH}/src/github.com/bikeshare_api
+NAMES=(${BINARY_NAMES//,/ })
+SIZE=${#NAMES[@]}
 
-# ブランチをチェック
-if [ -e $HOME ]; then
-    cd $HOME
-    git checkout .
-    git pull origin master
-else
-    cd ${GOPATH}/src/github.com
-    git clone https://github.com/8245snake/bikeshare_api.git
-fi
-chmod -R 777 $HOME
+# gitフォルダをチェックしあればpull、なければcloneする
+SourcePrepare(){
+    if [ -e $HOME ]; then
+        cd $HOME
+        git checkout .
+        git pull origin master
+    else
+        cd ${GOPATH}/src/github.com
+        git clone https://github.com/8245snake/bikeshare_api.git
+    fi
+    chmod -R 777 $HOME
+}
 
-# ビルドする
-cd ${HOME}/src/${BINARY_NAME}
-make
-
-# ビルド成果物を移動
-chmod -R 777 $HOME
-cp -R -f  ${HOME}/ /usr/
+# 全部ビルドする
+FullBuild(){
+    for BINARY_NAME in ${NAMES[@]}; do
+        cd ${HOME}/src/${BINARY_NAME}
+        godep restore
+        go build -o ${BINARY_NAME}
+        mv ${BINARY_NAME} ../../app/bin
+    done
+    # ビルド成果物を作業フォルダにコピー
+    chmod -R 777 $HOME
+    cp -R -f  ${HOME}/ /usr/
+}
 
 # 実行
-cd /usr/bikeshare_api/app/bin
-exec ./${BINARY_NAME}
+Execute(){
+    cd /usr/bikeshare_api/app/bin
+    exec ./${NAMES[0]}
+}
+
+# メイン処理
+if [ $SIZE = 1 ]; then
+    # 実行
+    Execute
+else
+    # BINARY_NAMESが複数あるときはビルドのみ
+    SourcePrepare
+    FullBuild
+fi
