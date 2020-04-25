d782ca50e5132b7c7e0c69369c0587115f91b0b3
Hanyu <8245snake@gmail.com>
2020-04-25 18:26:09 +0900

    ビルド環境を整理 実行環境用のdocker-composeも作成

--- build/build.sh
+++ build/build.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# イメージがない場合はビルド
+ARRAY=(`docker images | grep go-build`)
+if [ ${#ARRAY[@]} = 0 ]; then
+    docker build --network=host -t go-build:latest .
+fi
+
+# ビルドして移動
+mkdir -p ./app/bin
 docker-compose up --build
+mv -f ./app/bin/* ../app/bin/
+rm ./app/bin/.gitkeep
+
+# 正しく移動できていればフォルダ削除できるはず
+rmdir ./app/bin
+rmdir ./app
