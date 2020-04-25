713bf5d7614f96120feb5eddb5dd78528e86bbb2
Hanyu <8245snake@gmail.com>
2020-04-26 00:41:39 +0900

    ビルド後にコンテナ停止

--- build/build.sh
+++ build/build.sh
@@ -5,13 +5,17 @@ ARRAY=(`docker images | grep go-build`)
 if [ ${#ARRAY[@]} = 0 ]; then
     docker build --network=host -t go-build:latest .
 fi
-
-# ビルドして移動
+# ビルドコンテナ開始
 mkdir -p ./app/bin
 docker-compose up --build
+# 運用中のコンテナ停止
+cd ../
+docker-compose down
+# 移動
+cd build
 mv -f ./app/bin/* ../app/bin/
-rm ./app/bin/.gitkeep
-
-# 正しく移動できていればフォルダ削除できるはず
+rm -f ./app/bin/.gitkeep
+chmod -R 777 ../app/bin/
+# 後片付け（正しく移動できていればフォルダ削除できるはず）
 rmdir ./app/bin
 rmdir ./app
