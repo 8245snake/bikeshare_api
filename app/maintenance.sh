dd0e492f7f1c27fa9ff31905f502301035283dce
Hanyu <8245snake@gmail.com>
2020-05-30 23:39:13 +0900

    ラッパースクリプトを修正

--- app/maintenance.sh
+++ app/maintenance.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-cd ./bin
+# バイナリが見つかるまでループ
+while true
+do
+    if [ -e /usr/bikeshare_api/app/bin/stationfiller ]; then
+        break
+    fi
+    echo "looking for stationfiller..."
+    sleep 10
+done
+
+
+cd /usr/bikeshare_api/app/bin
 
 ./archiver &
 ./stationfiller &
